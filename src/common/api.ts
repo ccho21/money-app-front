2d342b8787c598b9a18cb6531ba8a930caaf6048
ccho21 <changsoo.charles.cho@gmail.com>
2025-04-24 19:38:15 -0400

    chore: add vercel proxy to backend

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,5 +1,7 @@
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
+// export const API_BASE_URL =
+//   process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
+
+export const API_BASE_URL = '/api';
 
 // ⛳ 에러 응답 핸들링
 async function handleResponse<T>(res: Response): Promise<T> {
