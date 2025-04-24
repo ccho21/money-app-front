96e02173876b8c54542d1c757038c82a42d09f35
ccho21 <changsoo.charles.cho@gmail.com>
2025-04-24 01:35:14 -0400

    fix: change the env variable name

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
 
 // ⛳ 에러 응답 핸들링
 async function handleResponse<T>(res: Response): Promise<T> {
